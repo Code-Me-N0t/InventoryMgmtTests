a45bf9960a992d2ad3668e254962516df1b4273e
Reden Longcop <ITA-40138@it-americano.local>
2024-09-11 18:55:08 +0800

    commit changes

--- InventoryMgmtQA/Service/TestTotalValue.cs
+++ InventoryMgmtQA/Service/TestTotalValue.cs
@@ -3,6 +3,8 @@ using InventoryMgmt.Service;
 using InventoryMgmt.Interface;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 // guide: https://www.aligrant.com/web/blog/2020-07-20_capturing_console_outputs_with_microsoft_test_framework
 
@@ -25,6 +27,37 @@ namespace InventoryMgmtQA.Service
             );
             _inventoryManager.GetTotalValue();
             Assert.IsTrue(sw.ToString().Contains("2.56"));
+        }
+
+        [TestMethod]
+        public void TestGetTotalValueUpdates()
+        {
+            var quantity1 = 3;
+            var price1 = 300;
+
+            var quantity2 = 2;
+            var price2 = 600;
+
+            using StringWriter sw = new();
+            Console.SetOut(sw);
+            _inventoryManager.AddNewProduct(
+                "TestProduct",
+                3,
+                300
+            );
+
+            _inventoryManager.AddNewProduct(
+                "TestProduct 2",
+                2,
+                600
+            );
+
+            var result1 = quantity1 * price1;
+            var result2 = quantity2 * price2;
+
+            var totalResult = Convert.ToDecimal(result1 + result2);
+
+            _inventoryManager.GetTotalValue();
         }
 
     }
